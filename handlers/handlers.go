ba756eb02a094dbb0e8b9ca0220554ee7a175bc7
Joao-Felisberto <joaofelisberto@ua.pt>
2024-03-24 19:19:11 +0000

    Site flow ready

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,6 +5,7 @@ package handlers
 import (
 	"github.com/Joao-Felisberto/devprivops-dashboard/data"
 	tpl "github.com/Joao-Felisberto/devprivops-dashboard/templates"
+	"github.com/Joao-Felisberto/devprivops-dashboard/util"
 	"github.com/labstack/echo"
 )
 
@@ -15,5 +16,24 @@ func Hello(c echo.Context) error {
 func ProjectsPage(store *data.Store) func(ctx echo.Context) error {
 	return func(ctx echo.Context) error {
 		return tpl.Page[*data.Report]("Projects Page", tpl.ProjectsPage, store.Data...).Render(ctx.Request().Context(), ctx.Response())
+	}
+}
+
+func RegulationsPage(store *data.Store) func(ctx echo.Context) error {
+	return func(ctx echo.Context) error {
+		project := ctx.Param("proj")
+		report := util.Filter(store.Data, func(r *data.Report) bool { return r.Project == project })[0]
+
+		return tpl.PageSingle[*data.Report]("Report Page", tpl.RegulationsPage, report).Render(ctx.Request().Context(), ctx.Response())
+	}
+}
+func PoliciesPage(store *data.Store) func(ctx echo.Context) error {
+	return func(ctx echo.Context) error {
+		project := ctx.Param("proj")
+		regName := ctx.Param("reg")
+		report := util.Filter(store.Data, func(r *data.Report) bool { return r.Project == project })[0]
+		regulation := util.Filter(report.Regulations, func(r *data.Regulation) bool { return r.Name == regName })[0]
+
+		return tpl.PageSingle[*data.Regulation]("Report Page", tpl.PoliciesPage, regulation).Render(ctx.Request().Context(), ctx.Response())
 	}
 }
--- main.go
+++ main.go
@@ -22,10 +22,59 @@ func main() {
 	store := data.Store{
 		Data: []*data.Report{
 			{
-				Branch:      "a",
-				Commit:      "a",
-				Project:     "a",
-				Regulations: []*data.Regulation{},
+				Branch:  "a",
+				Commit:  "a",
+				Project: "a",
+				Regulations: []*data.Regulation{
+					{
+						Name: "Reg 1",
+						ConsistencyResults: []*data.RuleResult{
+							{
+								Name: "Con 1",
+								Results: []map[string]interface{}{
+									{
+										"a": 1,
+										"b": 2,
+									},
+								},
+							},
+							{
+								Name: "Con 2",
+								Results: []map[string]interface{}{
+									{
+										"c": 3,
+									},
+								},
+							},
+						},
+						PolicyResults: []*data.RuleResult{
+							{
+								Name: "Pol 1",
+								Results: []map[string]interface{}{
+									{
+										"a": 'a',
+										"b": 'b',
+										"c": 'c',
+									},
+								},
+							},
+						},
+					},
+					{
+						Name: "Reg 2",
+						ConsistencyResults: []*data.RuleResult{
+							{
+								Name: "Con 2",
+								Results: []map[string]interface{}{
+									{
+										"Something": 1,
+										"Another":   999,
+									},
+								},
+							},
+						},
+					},
+				},
 			},
 			{
 				Branch:      "b",
@@ -38,6 +87,8 @@ func main() {
 
 	e.Static("/static", "static")
 	e.GET("/", handlers.ProjectsPage(&store))
+	e.GET("/:proj", handlers.RegulationsPage(&store))
+	e.GET("/:proj/:reg", handlers.PoliciesPage(&store))
 
 	e.Logger.Fatal(e.Start("localhost:8080"))
 }
